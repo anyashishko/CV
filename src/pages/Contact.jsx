cbba6da245a1641cb79127bffe4504666b971550
Hanna Shyshko <anyashisha6@gmail.com>
2024-12-21 22:41:27 +0300

    Update Contact.jsx

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -7,8 +7,8 @@ const Contact = () => {
       <h2>Contact</h2>
       <p>You can reach me through the following social media or email:</p>
       <ul className={styles.contactList}>
-        <li><a href="mailto:example@example.com" className={styles.contactItem}>Email: example@example.com</a></li>
-        <li><a href="https://www.instagram.com/username" target="_blank" rel="noopener noreferrer" className={styles.contactItem}>Instagram</a></li>
+        <li><a href="mailto:anyashisha6@gmail.com" className={styles.contactItem}>anyashisha6@gmail.com</a></li>
+        <li><a href="https://www.instagram.com/aanyaa____/" target="_blank" rel="noopener noreferrer" className={styles.contactItem}>Instagram</a></li>
         <li><a href="https://t.me/username" target="_blank" rel="noopener noreferrer" className={styles.contactItem}>Telegram</a></li>
       </ul>
     </section>
